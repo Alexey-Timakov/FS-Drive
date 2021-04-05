c00cac838760441bd6f75bd771de0c9cfb92919d
Alexey T <avtimakov70@gmail.com>
2021-04-06 00:42:11 +0700

    SF-Module 19: Added animation into Q-A section

--- faq_script.js
+++ faq_script.js
@@ -3,7 +3,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].classList.toggle("active");
-        buttons[i].nextElementSibling.style.display = "none";
+        // buttons[i].nextElementSibling.style.display = "none";
+        buttons[i].nextElementSibling.style.maxHeight = "0px";
         buttons[i].querySelector(".arrow_up").classList.toggle("active");
         buttons[i].querySelector(".arrow_down").classList.toggle("active");
 
@@ -13,12 +14,12 @@ document.addEventListener("DOMContentLoaded", function() {
             let arrowDown = this.querySelector(".arrow_down");
             let arrowUp = this.querySelector(".arrow_up");
 
-            if (answerDiv.style.display == "block") {
-                answerDiv.style.display = "none";
+            if (answerDiv.style.maxHeight != "0px") {
+                answerDiv.style.maxHeight = "0px";
                 arrowDown.classList.toggle("active");
                 arrowUp.classList.toggle("active");
             } else {
-                answerDiv.style.display = "block";
+                answerDiv.style.maxHeight = answerDiv.scrollHeight + "px";
                 arrowDown.classList.toggle("active");
                 arrowUp.classList.toggle("active");
             }
