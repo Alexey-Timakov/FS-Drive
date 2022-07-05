b6320d614480b1eb52955e9cc092420f0d42309f
Alexey T <avtimakov70@gmail.com>
2022-07-06 01:25:23 +0700

    Fixied git folder issues, removed trash files and folders

--- SkillDrive/FrontEnd/src/components/App.tsx
+++ SkillDrive/FrontEnd/src/components/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-// import {BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
 import {HashRouter as Router, Route, Switch, Link} from "react-router-dom";
 
 import About from "./About/About";
